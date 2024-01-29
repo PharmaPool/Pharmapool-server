5ddda507c9130c8d995784c97d945ea74552816b
Pharmapool <pharmapoolng@gmail.com>
2024-01-29 16:40:21 +0100

    completed the code snippets for the user profile

--- controller/profile.js
+++ controller/profile.js
@@ -1,6 +1,6 @@
 const User = require("../model/user");
 
-const { getUser } = require("../util/user");
+const { getUser, userExists } = require("../util/user");
 const error = require("../util/error-handling/errorHandler");
 const { uploadImage, removeImage } = require("../util/images/images");
 
@@ -51,6 +51,122 @@ module.exports.getProfileDetails = async (req, res, next) => {
     res
       .status(200)
       .json({ message: "user details fetched successfully", user });
+  } catch (err) {
+    error.error(err, next);
+  }
+};
+
+/************************
+ * Update User Fullname *
+ ************************/
+module.exports.updateUserFullname = async (req, res, next) => {
+  const userId = req.params.userId,
+    firstName = req.body.firstName,
+    lastName = req.body.lastName;
+
+  try {
+    // Get and validate user
+    const user = await getUser(userId);
+
+    if (!user) error.errorHandler(res, "user not found", "user");
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+
+    await user.save();
+
+    // Return response to client
+    res
+      .status(200)
+      .json({ message: "profile updated successfully", updated: user.details });
+  } catch (err) {
+    error.error(err, next);
+  }
+};
+
+/**************************
+ * Update Profile Details *
+ **************************/
+module.exports.updateProfileDetails = async (req, res, next) => {
+  const userId = req.params.userId,
+    email = req.body.email,
+    about = req.body.about,
+    state = req.body.state,
+    address = req.body.address,
+    phoneNumber = req.body.phone;
+
+  try {
+    // Get and validate user
+    const user = await getUser(userId);
+
+    if (!user) error.errorHandler(res, "user not found", "user");
+
+    user.details.email = email;
+    user.details.about = about;
+    user.details.state = state;
+    user.details.address = address;
+    user.details.phoneNumber = phoneNumber;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "profile updated successfully", updated: user.details });
+  } catch (err) {
+    error.error(err, next);
+  }
+};
+
+module.exports.changeImage = async (req, res, next) => {
+  const userId = req.params.userId,
+    type = req.body.type;
+
+  try {
+    const uploadedImage = await uploadImage(req.file.path);
+
+    // Get current user
+    const user = await User.findById(userId);
+
+    // Check if user exists
+    if (!user) error.errorHandler(res, "user not found", "user");
+
+    if (!uploadedImage.imageUrl)
+      error.errorHandler(res, "image not uploaded", "image");
+
+    // Continue if no errors
+
+    let public_id;
+    if (type === "profile") {
+      public_id = user.profileImage.imageId;
+    } else if (type === "banner") {
+      public_id = user.bannerImage.imageId;
+    }
+
+    let imageUrl, imageId;
+    if (uploadedImagge) {
+      imageUrl = uploadedImage.imageUrl;
+      imageId = uploadedImage.imageId;
+    }
+
+    switch (type) {
+      case "profile":
+        user.profileImage.imageUrl = `${imageUrl}`;
+        user.profileImage.imageId = imageId;
+        removeImage(res, public_id);
+        break;
+
+      case "banner":
+        user.bannerImage.imageUrl = `${imageUrl}`;
+        user.bannerImage.imageUrl = imageId;
+        removeImage(res, public_id);
+
+      default:
+        return;
+    }
+
+    //   Save user to database
+    await user.save();
+    res.status(200).json({ message: "image updated", user });
   } catch (err) {
     error.error(err, next);
   }
